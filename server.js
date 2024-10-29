fffa6a81649ec2bb9f31d1644a6abcf59d4f9439
ryandeist <ryandeist@gmail.com>
2024-10-28 20:23:55 -0400

    index.ejs setup complete

--- server.js
+++ server.js
@@ -3,6 +3,11 @@
 const express = require("express");
 
 const app = express();
+const PORT = process.env.PORT ? process.env.PORT : '3000' ; 
+
+app.get('/', async(req, res) => {
+    res.render('index.ejs');
+});
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
