2781fc9dfe968520f2301577361eff51d564c18d
ryandeist <ryandeist@gmail.com>
2024-10-31 20:34:34 -0400

    added update and delete routes

--- server.js
+++ server.js
@@ -6,14 +6,18 @@ const express = require("express");
 
 const Fruit = require("./models/fruit");
 const app = express();
-const PORT = process.env.PORT ? process.env.PORT : '3000' ;
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+const PORT = process.env.PORT ? process.env.PORT : '3000';
 
 //middleware
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use(morgan("dev"));
 
 // routes
 app.get('/', async (req, res) => {
-    res.render('index.ejs');
+  res.render('index.ejs');
 });
 // setup a route to show a form to add a fruit
 // server.js
@@ -28,7 +32,7 @@ app.get("/fruits", async (req, res) => {
 app.get("/fruits/new", (req, res) => {
   res.render("fruits/new.ejs");
 });
-
+// GET fruit by ID
 app.get("/fruits/:fruitId", async (req, res) => {
   const foundFruit = await Fruit.findById(req.params.fruitId);
   res.render("fruits/show.ejs", { fruit: foundFruit });
@@ -47,6 +51,34 @@ app.post("/fruits", async (req, res) => {
   res.redirect("/fruits"); // redirect to index fruits
 });
 
+app.delete("/fruits/:fruitId", async (req, res) => {
+  await Fruit.findByIdAndDelete(req.params.fruitId);
+  res.redirect("/fruits");
+});
+
+// GET localhost:3000/fruits/:fruitId/edit
+app.get("/fruits/:fruitId/edit", async (req, res) => {
+  const foundFruit = await Fruit.findById(req.params.fruitId);
+  res.render("fruits/edit.ejs", {
+    fruit: foundFruit,
+  });
+});
+
+
+app.put("/fruits/:fruitId", async (req, res) => {
+  // Handle the 'isReadyToEat' checkbox data
+  if (req.body.isReadyToEat === "on") {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
+  
+  // Update the fruit in the database
+  await Fruit.findByIdAndUpdate(req.params.fruitId, req.body);
+
+  // Redirect to the fruit's show page to see the updates
+  res.redirect(`/fruits/${req.params.fruitId}`);
+});
 
 
 // setup a route to show a form to update a fruit
